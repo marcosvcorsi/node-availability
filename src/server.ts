72070d2022f3511c347f1be6403373726ea7a2d5
Marcos Corsi <marcosvcorsi@gmail.com>
2022-01-27 22:29:18 -0300

    chore: testing timezones

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
+import dayjsUTC from "dayjs/plugin/utc";
+import dayjsTimezone from "dayjs/plugin/timezone";
+import dayjsAdvancedFormat from "dayjs/plugin/advancedFormat";
+
+dayjs.extend(dayjsUTC);
+dayjs.extend(dayjsTimezone);
+dayjs.extend(dayjsAdvancedFormat);
 
 const app = express();
 
@@ -24,9 +32,37 @@ app.post("/", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-  const availabilities = await prismaClient.availability.findMany();
+  const timeZone = (req.query.timeZone ?? "America/New_York") as string;
+  const date = (req.query.date ?? "2022-01-28") as string;
 
-  return availabilities;
+  const utcOffset = dayjs().tz(timeZone).utcOffset();
+
+  console.log({ utcOffset, timeZone });
+
+  const availability = await prismaClient.availability.findFirst();
+
+  if (!availability) {
+    return res.status(400).json({ message: "There is no availability" });
+  }
+
+  const startDateTime = dayjs(date)
+    .set("hour", availability.startTime.getHours())
+    .set("minute", availability.startTime.getMinutes())
+    .toDate();
+
+  const endDateTime = dayjs(date)
+    .set("hour", availability.endTime.getHours())
+    .set("minute", availability.endTime.getMinutes())
+    .toDate();
+
+  console.log({ startDateTime, endDateTime });
+
+  const startTime = dayjs.tz(startDateTime).tz(timeZone, true).utc().format();
+  const endTime = dayjs.tz(endDateTime, timeZone).utc().format();
+
+  console.log({ startTime, endTime });
+
+  return res.json({ startTime, endTime, timeZone });
 });
 
 app.listen(3000, () => console.log("Server is running"));
